df5bb09c01bcc408541a252bdf4c2d1c00855b37
nckedev <nmk411@gmail.com>
2022-09-14 07:04:06 +0200

    codefix project

---
+++ analyzerCodeFix/FunctionNameCodeFix.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+
+namespace analyzerCodeFix;
+
+[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(FunctionNameCodeFix)), Shared]
+public class FunctionNameCodeFix : CodeFixProvider
+{
+    public override ImmutableArray<string> FixableDiagnosticIds { get; } = ImmutableArray.Create("SLH001");
+
+    public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+    {
+        var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+        var diagnostic = context.Diagnostics.First();
+        context.RegisterCodeFix(CodeAction.Create(title: "test", c => CreateNewDocument(), equivalenceKey: "test2"),
+            diagnostic);
+    }
+
+    private static async Task<Document> CreateNewDocument()
+    {
+        return null;
+    }
+}
